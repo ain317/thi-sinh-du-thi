465f032936f7332ed868248c514d721d2ca826a2
ain317 <huya3107@gmail.com>
2025-05-06 10:58:03 +0700

    Add files via upload

--- Thisinh.hpp
+++ Thisinh.hpp
@@ -1,15 +1,23 @@
 #ifndef THISINH_H
 #define THISINH_H
 #include <iostream>
+#include <sstream>
 using namespace std;
+bool CheckName( string a){
+    stringstream  cw(a);
+    string cn;
+    while( cw >> cn){
+        for ( char Ktra : cn){
+            if (!isalpha(Ktra)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 class ThiSinh {
-
-    
-    
-    
-    
-    protected:
+    protected:            // hàm để kế thừa
         string soBaoDanh;
         string hoTen;
         string diaChi;
@@ -17,19 +25,37 @@ class ThiSinh {
     
     public:
         ThiSinh() {}
-        ThiSinh(string sbd, string ht, string dc, int uutien)
-            : soBaoDanh(sbd), hoTen(ht), diaChi(dc), mucUuTien(uutien) {}
+        ThiSinh(string sbd, string ht, string dc, int uutien){
+            soBaoDanh = sbd;
+            hoTen = ht;
+            diaChi = dc;
+            mucUuTien = uutien;
+            }
     
-        virtual void nhap() {
+        virtual void nhap() { //void là hàm không trả về giá trị, virtual là hàm ảo không thể sử dụng ngoài class, có thể kế thừa và ghi đè        
             cout << "Nhap so bao danh: ";
             getline(cin, soBaoDanh);
-            cout << "Nhap ho ten: ";
-            getline(cin, hoTen);
+
+            while (true){
+
+        
+    
+        
+                cout << "Nhap ho ten: ";
+        
+                getline(cin, hoTen);
+                if(CheckName(hoTen)){
+                    break;
+                }else{
+                    continue;
+                }
+            }
+          
             cout << "Nhap dia chi: ";
             getline(cin, diaChi);
             cout << "Nhap muc uu tien: ";
             cin >> mucUuTien;
-            cin.ignore();
+            cin.ignore(); // cin.ignore là bỏ qua giá trị nhập vào
         }
     
         virtual void hienThi() const {
@@ -41,6 +67,7 @@ class ThiSinh {
             return soBaoDanh;
         }
     
-        virtual ~ThiSinh() {}
+        virtual ~ThiSinh() {} //~ là hàm huỷ
     };
+    
     #endif
